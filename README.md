8634e9fb97aad7c5885ff2d99859e18917aef7c1
sachin.jadhav222 <sachin.jadhav222@yahoo.com>
2021-02-16 15:42:18 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# what is this
+This is JS shadow package 
+
+# installation
+npm i shadowsac --save
+
+Then....
+
+```
+import {shadowsac} from 'shadowsac;
+
+shadowsac({
+    shadow_type: 'soft',
+    padding: false
+})
+
+```
+# Options 
---
+++ index.js
@@ -0,0 +1,20 @@
+function shadowsac(options){
+    let imgages= document.querySelectorAll('.shadowizard')
+
+    if(options.shadow_types === 'hard')
+     options.shadow_types='0px'  
+     
+     else
+     options.shadow_types='15px'
+
+     imgages.forEach(image=>{
+         image.getElementsByClassName.boxShadow= `10px 10px ${options.shadow_types} 1px rgba(0,0,0,0.12)`;
+
+         if(options.padding){
+             image.getElementsByClassName.padding='1em'
+         }
+     })
+
+}
+
+module.exports.shadowsac=shadowsac;
